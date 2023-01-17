396cd921074588d8ac5c4055de871ad83c86de46
Mario Bellido Jiménez <mariobj1999@gmail.com>
2023-01-17 16:42:51 +0100

    Añadiendo funcionalidad al Core

--- INTERMODULAR/Core/RelayCommand.cs
+++ INTERMODULAR/Core/RelayCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace INTERMODULAR.Core
+namespace INTERMODULAR.Core //Cambios en el relay
 {
     class RelayCommand : ICommand
     {
