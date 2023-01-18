80fc1b8f130cc68281fca196966e6fc08b75d91f
Mario Bellido Jiménez <mariobj1999@gmail.com>
2023-01-18 17:23:34 +0100

    Implementado commando de inicio de sesión que comprueba si los datos son validos

---
+++ INTERMODULAR/MVVM/ViewModel/LoginViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace INTERMODULAR.MVVM.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        public string Username 
+        { 
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public SecureString Password 
+        { 
+            get => _password;
+            set 
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+
+            }
+        }
+
+        public string ErrorMessage 
+        { 
+            get => _errorMessage; 
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+
+            }
+
+        }
+
+        public bool IsViewVisible 
+        { 
+            get => _isViewVisible; 
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverPassCommand(""));
+        }
+
+        private void ExecuteRecoverPassCommand(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if(
+                string.IsNullOrEmpty(Username) || Username.Length <3 ||
+                Password == null || Password.Length < 3
+            )
+            {
+                validData = false;
+            }
+            else
+            {
+                validData = true;
+            }
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
