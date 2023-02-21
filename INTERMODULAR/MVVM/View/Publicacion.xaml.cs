e45f89b2ed0b796fe27c887885880e83c1875100
Mario Bellido Jiménez <mariobj1999@gmail.com>
2023-02-21 13:01:36 +0100

    Finalizada publicacion

---
+++ INTERMODULAR/MVVM/View/Publicacion.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INTERMODULAR.MVVM.View
+{
+    /// <summary>
+    /// Lógica de interacción para Publicacion.xaml
+    /// </summary>
+    public partial class Publicacion : UserControl
+    {
+        public Publicacion(string id, string titulo, string foto, string usuario, string fecha, string descripcion, string categoria)
+        {
+            InitializeComponent();
+            b_titulo.Text = titulo;
+            b_usuario.Text = usuario;
+            b_fecha.Text = fecha;
+            b_descripcion.Text = descripcion;
+            b_categoria.Text = categoria;
+        }
+    }
+}
